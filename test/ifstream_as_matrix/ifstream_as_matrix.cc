1460e05ff0ee5f61bb116f84c18a03b3950e2e46
David Gleich <dgleich@stanford.edu>
2006-03-19 19:33:33 +0000

    Update of ifstream matrix test

--- test/ifstream_as_matrix/ifstream_as_matrix.cc
+++ test/ifstream_as_matrix/ifstream_as_matrix.cc
@@ -19,6 +19,36 @@
 #include <yasmic/binary_ifstream_matrix.hpp>
 #include <yasmic/cluto_ifstream_matrix.hpp>
 
+#include <yasmic/transpose_matrix.hpp>
+#include <yasmic/nonzero_union.hpp>
+
+template <class Matrix>
+void dump_matrix(Matrix& m)
+{
+	using namespace yasmic;
+	using namespace std;
+
+	typedef smatrix_traits<Matrix>::size_type size_type;
+
+		size_type nr = nrows(m);
+		size_type nc = ncols(m);
+		cout << "nrows: " << nr << endl;
+		cout << "ncols: " << nc << endl;
+	    
+		{
+    		smatrix_traits<Matrix>::nonzero_iterator nzi, nzend;
+	    
+    		tie(nzi, nzend) = nonzeros(m);
+	    	
+    		for (; nzi != nzend; ++nzi)
+    		{
+				cout << row(*nzi, m) << " " << column(*nzi, m) << " " << value(*nzi, m) << endl;
+    		}
+
+		}
+}
+
+
 int main(int argc, char **argv)
 {
     using namespace std;
@@ -40,81 +70,49 @@ int main(int argc, char **argv)
 	typedef binary_ifstream_matrix<> simple_bin_matrix;
 	typedef cluto_ifstream_matrix<> cluto_matrix;
 
+	//typedef add_symmetric<simple_matrix> symmetric_matrix;
+
     
     ifstream f(filename_txt.c_str());
-    simple_matrix m(f);
+    //symmetric_matrix m = static_cast<symmetric_matrix>(simple_matrix(f));
+	simple_matrix m(f);
+
+	size_type nr, nc;
+	boost::tie(nr, nc) = dimensions(m);
 
 	ifstream b(filename_bin.c_str(), ios::binary);
 	simple_bin_matrix m2(b);
 
+	ifstream b2(filename_bin.c_str(), ios::binary);
+	simple_bin_matrix m22(b2);
+
 	ifstream c(filename_cluto.c_str());
 	cluto_matrix m3(c);
     
-    
-	{
-		size_type nr = nrows(m);
-		size_type nc = ncols(m);
-		cout << "nrows: " << nr << endl;
-		cout << "ncols: " << nc << endl;
-	    
-		{
-    		smatrix_traits<simple_matrix>::nonzero_iterator nzi, nzend;
-	    
-    		tie(nzi, nzend) = nonzeros(m);
-	    	
-    		for (; nzi != nzend; ++nzi)
-    		{
-				cout << row(*nzi, m) << " " << column(*nzi, m) << " " << value(*nzi, m) << endl;
-    		}
 
-		}
-	}
+	dump_matrix(m);
+	dump_matrix(m2);
 
-	{
-		size_type nr = nrows(m2);
-		size_type nc = ncols(m2);
-		cout << "nrows: " << nr << endl;
-		cout << "ncols: " << nc << endl;
+	istringstream _line("12 15 1\n5 6 8\n");
 
-		cout << "nnz: " << nnz(m2) << endl;
-	    
-		{
-    		smatrix_traits<simple_bin_matrix>::nonzero_iterator nzi, nzend;
-	    
-    		tie(nzi, nzend) = nonzeros(m2);
-	    	
-    		for (; nzi != nzend; ++nzi)
-    		{
-				cout << row(*nzi, m2) << " " << column(*nzi, m2) << " " << value(*nzi, m2) << endl;
-    		}
+	simple_matrix mtest(_line);
+	
+	dump_matrix(mtest);
+    dump_matrix(m3);
 
-		}
-	}
+	
 
-	istringstream _line("123 456 789     10 11 12");
+	typedef transpose_matrix<simple_bin_matrix> tmatrix;
 
-    int i;
-	_line >> i;
-	{
-		size_type nr = nrows(m3);
-		size_type nc = ncols(m3);
-		cout << "nrows: " << nr << endl;
-		cout << "ncols: " << nc << endl;
+	typedef nonzero_union<simple_bin_matrix, tmatrix> nzu_matrix;
 
-		cout << "nnz: " << nnz(m3) << endl;
-	    
-		{
-    		smatrix_traits<cluto_matrix>::nonzero_iterator nzi, nzend;
-	    
-    		tie(nzi, nzend) = nonzeros(m3);
-	    	
-    		for (; nzi != nzend; ++nzi)
-    		{
-				cout << row(*nzi, m3) << " " << column(*nzi, m3) << " " << value(*nzi, m3) << endl;
-    		}
+	tmatrix trans_m(m2);
 
-		}
-	}
+	nzu_matrix nzu(m22, trans_m);
+
+	//cout << "nrows: " << nrows(trans_m) << " ncols: " << ncols(trans_m) << endl;
+
+	dump_matrix(nzu);
     
     return (0);
 }
