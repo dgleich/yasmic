c5a8bd283412e1884a0ceae4c4c03b4f332b1c3e
David Gleich <dgleich@stanford.edu>
2007-07-30 18:47:07 +0000

    Initial commit of core numbers with visitors

--- yasmic/boost_mod/core_numbers.hpp
+++ yasmic/boost_mod/core_numbers.hpp
@@ -12,13 +12,19 @@
 #define BOOST_GRAPH_CORE_NUMBERS_HPP
 
 #include <boost/pending/mutable_queue.hpp>
+#include <boost/graph/breadth_first_search.hpp>
 
 /*
- *KCore
+ *core_numbers
  *
  *Requirement:
  *      IncidenceGraph
  */
+
+// History
+//
+// 30 July 2007
+// Added visitors to the implementation
 
 namespace boost {
 
@@ -31,6 +37,51 @@ namespace boost {
     // The linear algorithm comes from:
     // Vladimir Batagelj and Matjaz Zaversnik, "An O(m) Algorithm for Cores 
     // Decomposition of Networks."  Sept. 1 2002.
+    
+    template <typename Visitor, typename Graph>
+    struct CoreNumbersVisitorConcept {
+        void constraints()
+        {
+            function_requires< CopyConstructibleConcept<Visitor> >();
+            vis.examine_vertex(u,g);
+            vis.finish_vertex(u,g);
+            vis.examine_edge(e,g);
+        }
+        Visitor vis;
+        Graph g;
+        typename graph_traits<Graph>::vertex_descriptor u;
+        typename graph_traits<Graph>::edge_descriptor e;
+    };
+    
+    template <class Visitors=null_visitor>
+    class core_numbers_visitor : public bfs_visitor<Visitors> {
+        public:
+        core_numbers_visitor() {}
+        core_numbers_visitor(Visitors vis) 
+            : bfs_visitor<Visitors>(vis) {}
+        
+        private:
+        template <class Vertex, class Graph>
+        void initialize_vertex(Vertex, Graph&) {}
+        template <class Vertex, class Graph>
+        void discover_vertex(Vertex , Graph&) {}
+        template <class Vertex, class Graph>
+        void gray_target(Vertex, Graph&) {}
+        template <class Vertex, class Graph>
+        void black_target(Vertex, Graph&) {}
+        template <class Edge, class Graph>
+        void tree_edge(Edge, Graph&) {}
+        template <class Edge, class Graph>
+        void non_tree_edge(Edge, Graph&) {}
+    };
+    
+    template <class Visitors>
+    core_numbers_visitor<Visitors>
+    make_core_numbers_visitor(Visitors vis) {
+        return core_numbers_visitor<Visitors>(vis);
+    };
+    typedef core_numbers_visitor<> default_core_numbers_visitor;
+            
 
     namespace detail {
         
@@ -76,10 +127,11 @@ namespace boost {
         
         // the version for weighted graphs is a little different
         template <typename Graph, typename CoreMap, 
-            typename EdgeWeightMap, typename MutableQueue>
+            typename EdgeWeightMap, typename MutableQueue,
+            typename Visitor>
         typename property_traits<CoreMap>::value_type
         core_numbers_impl(Graph& g, CoreMap c, EdgeWeightMap wm,
-            MutableQueue& Q)
+            MutableQueue& Q, Visitor vis)
         { 
             typename property_traits<CoreMap>::value_type v_cn = 0;
             typedef typename graph_traits<Graph>::vertex_descriptor vertex;
@@ -88,10 +140,12 @@ namespace boost {
                 // remove v from the Q, and then decrease the core numbers 
                 // of its successors
                 vertex v = Q.top(); 
+                vis.examine_vertex(v,g);
                 Q.pop();
                 v_cn = get(c,v);
                 typename graph_traits<Graph>::out_edge_iterator oi,oi_end;
                 for (tie(oi,oi_end) = out_edges(v,g); oi!=oi_end; ++oi) {
+                    vis.examine_edge(*oi,g);
                     vertex u = target(*oi,g);
                     // if c[u] > c[v], then u is still in the graph,
                     if (get(c,u) > v_cn) {
@@ -100,15 +154,16 @@ namespace boost {
                         Q.update(u);
                     }
                 }
+                vis.finish_vertex(v,g);
             }
             return (v_cn);
         }
         
         template <typename Graph, typename CoreMap, typename EdgeWeightMap,
-            typename IndexMap>
+            typename IndexMap, typename CoreNumVisitor>
         typename property_traits<CoreMap>::value_type 
         core_numbers_dispatch(Graph&g, CoreMap c, EdgeWeightMap wm,
-            IndexMap im)
+            IndexMap im, CoreNumVisitor vis)
         {
             typedef typename property_traits<CoreMap>::value_type D;
             typedef std::less<D> Cmp;
@@ -123,15 +178,16 @@ namespace boost {
             for (tie(vi,vi_end) = vertices(g); vi!=vi_end; ++vi) { 
                 Q.push(*vi);
             }
-            return core_numbers_impl(g, c, wm, Q);
+            return core_numbers_impl(g, c, wm, Q, vis);
         }
         
         // the version for the unweighted case
         // for this functions CoreMap must be initialized
         // with the in degree of each vertex
-        template <typename Graph, typename CoreMap, typename PositionMap>
+        template <typename Graph, typename CoreMap, typename PositionMap,
+            typename Visitor>
         typename property_traits<CoreMap>::value_type
-        core_numbers_impl(Graph& g, CoreMap c, PositionMap pos)
+        core_numbers_impl(Graph& g, CoreMap c, PositionMap pos, Visitor vis)
         {
             typedef typename graph_traits<Graph>::vertices_size_type size_type;
             typedef typename graph_traits<Graph>::degree_size_type degree_type;
@@ -178,9 +234,11 @@ namespace boost {
             // now simulate removing the vertices
             for (size_type i=0; i < num_vertices(g); ++i) {
 			    vertex v = vert[i];
+                vis.examine_vertex(v,g);
                 v_cn = get(c,v);
                 typename graph_traits<Graph>::out_edge_iterator oi,oi_end;
                 for (tie(oi,oi_end) = out_edges(v,g); oi!=oi_end; ++oi) {
+                    vis.examine_edge(*oi,g);
                     vertex u = target(*oi,g);
                     // if c[u] > c[v], then u is still in the graph,
                     if (get(c,u) > v_cn) {
@@ -208,15 +266,17 @@ namespace boost {
 					    put(c,u,get(c,u)-1);
                     }
                 }
+                vis.finish_vertex(v,g);
             }
             return v_cn;
         }
 
     } // namespace detail
 
-    template <typename Graph, typename CoreMap>
+    // non-named parameter version for the unweighted case
+    template <typename Graph, typename CoreMap, typename CoreNumVisitor>
     typename property_traits<CoreMap>::value_type
-    core_numbers(Graph& g, CoreMap c)
+    core_numbers(Graph& g, CoreMap c, CoreNumVisitor vis)
     {
         typedef typename graph_traits<Graph>::vertices_size_type size_type;
         detail::compute_in_degree_map(g,c,
@@ -224,34 +284,54 @@ namespace boost {
                 typename property_traits<CoreMap>::value_type>(1) );
         return detail::core_numbers_impl(g,c,
             make_iterator_property_map(
-                std::vector<size_type>(num_vertices(g)).begin(),get(vertex_index, g))
+                std::vector<size_type>(num_vertices(g)).begin(),get(vertex_index, g)), 
+            vis
         );
     }
     
-    template <typename Graph, typename CoreMap, typename EdgeWeightMap,
-        typename VertexIndexMap >
+    // non-named paramter version for the unweighted case
+    template <typename Graph, typename CoreMap>
     typename property_traits<CoreMap>::value_type
-    core_numbers(Graph& g, CoreMap c, EdgeWeightMap wm, VertexIndexMap vim)
+    core_numbers(Graph& g, CoreMap c)
     {
-        typedef typename graph_traits<Graph>::vertices_size_type size_type;
-        detail::compute_in_degree_map(g,c,wm);
-        return detail::core_numbers_dispatch(g,c,wm,vim);
+        return core_numbers(g, c, make_core_numbers_visitor(null_visitor()));
     }
     
-    template <typename Graph, typename CoreMap, typename EdgeWeightMap>
+    // non-named parameter version for the weighted case
+    template <typename Graph, typename CoreMap, typename EdgeWeightMap,
+        typename VertexIndexMap, typename CoreNumVisitor>
     typename property_traits<CoreMap>::value_type
-    core_numbers(Graph& g, CoreMap c, EdgeWeightMap wm)
+    core_numbers(Graph& g, CoreMap c, EdgeWeightMap wm, VertexIndexMap vim,
+        CoreNumVisitor vis)
     {
         typedef typename graph_traits<Graph>::vertices_size_type size_type;
         detail::compute_in_degree_map(g,c,wm);
-        return detail::core_numbers_dispatch(g,c,wm,get(vertex_index,g));
+        return detail::core_numbers_dispatch(g,c,wm,vim,vis);
     }
+    
+    // non-named parameter version for the weighted case
+//    template <typename Graph, typename CoreMap, typename EdgeWeightMap>
+//    typename property_traits<CoreMap>::value_type
+//    core_numbers(Graph& g, CoreMap c, EdgeWeightMap wm)
+//    {
+//        typedef typename graph_traits<Graph>::vertices_size_type size_type;
+//        detail::compute_in_degree_map(g,c,wm);
+//        return detail::core_numbers_dispatch(g,c,wm,get(vertex_index,g),
+//            make_core_numbers_visitor(null_visitor()));
+//    }
     
     template <typename Graph, typename CoreMap>
     typename property_traits<CoreMap>::value_type
     weighted_core_numbers(Graph& g, CoreMap c)
     {
         return core_numbers(g,c,get(edge_weight,g));
+    }
+    
+    template <typename Graph, typename CoreMap, typename CoreNumVisitor>
+    typename property_traits<CoreMap>::value_type
+    weighted_core_numbers(Graph& g, CoreMap c, CoreNumVisitor vis)
+    {
+        return core_numbers(g,c,get(edge_weight,g),get(vertex_index,g),vis);
     }
 
 } // namespace boost
